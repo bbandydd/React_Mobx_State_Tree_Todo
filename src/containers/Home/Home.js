e4f2786cd74229055b9c2f82724d6ac225ea8030
Andy Tsai <bbandydd@gmail.com>
2017-10-01 15:43:34 +0800

    add count store

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observable, action } from 'mobx';
 import { observer, inject } from 'mobx-react';
+import { types } from 'mobx-state-tree';
+
+const CountStore = types
+  .model('Count', {
+    number: 0,
+  })
+  .actions(self => ({
+    increment() {
+      self.number += 1;
+    },
+    decrement() {
+      self.number -= 1;
+    }
+  }));
 
 @inject('TodoStore')
 @observer
@@ -11,6 +25,7 @@ export default class Home extends Component {
   }
 
   @observable newTodoTitle = '';
+  @observable countStore = CountStore.create();
 
   @action
   handleChange = (e) => {
@@ -28,24 +43,33 @@ export default class Home extends Component {
 
     return (
       <div>
-        <input value={this.newTodoTitle} onChange={this.handleChange} />
-        <button onClick={this.handleNewTodoClick}>Add</button>
-        <ul>
+        <div>
           {
-            TodoStore.todos
-              .values()
-              .map((todo, index) => (
-                <li key={index}>
-                  <input
-                    type="checkbox"
-                    checked={todo.finished}
-                    onClick={todo.toggle}
-                  />
-                  { todo.title }
-                </li>
-              ))
+            this.countStore.number
           }
-        </ul>
+          <button onClick={this.countStore.increment}>+</button>
+          <button onClick={this.countStore.decrement}>-</button>
+        </div>
+        <div>
+          <input value={this.newTodoTitle} onChange={this.handleChange} />
+          <button onClick={this.handleNewTodoClick}>Add</button>
+          <ul>
+            {
+              TodoStore.todos
+                .values()
+                .map((todo, index) => (
+                  <li key={index}>
+                    <input
+                      type="checkbox"
+                      checked={todo.finished}
+                      onClick={todo.toggle}
+                    />
+                    { todo.title }
+                  </li>
+                ))
+            }
+          </ul>
+        </div>
       </div>
     );
   }
